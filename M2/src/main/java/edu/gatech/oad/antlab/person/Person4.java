ad82f3d0212bdce1b241d0f9450aa4bdea12f132
Alex Gurney <thaeds@gmail.com>
2015-09-04 21:14:16 -0400

    Added implementation of Person4.calc(String)

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Alex Gurney
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,11 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      StringBuffer ret = new StringBuffer();
+      for(int i = 0; i < input.length(); i++) {
+        ret.append(input[i] + 1);
+      }
+      return ret.toString();
     }
     
     /**
