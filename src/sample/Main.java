0d11bb64fdc997653646f9209fb9e6e1362508c2
Alex Gurney <thaeds@gmail.com>
2015-12-01 20:15:29 -0500

    Added an attempt at music... doesn't work on my computer, but it should work...

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,12 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
 public class Main extends Application {
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        // Attempt to add music
+//        String musicPath = "pub.mp3";
+//        File musicFile = new File(musicPath);
+//        Media media = new Media(musicFile.toURI().toASCIIString());
+//        System.out.println(media.getSource());
+//        MediaPlayer player = new MediaPlayer(media);
+//        player.play();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
         Parent root = (Parent) loader.load();
         Controller control = loader.getController();
